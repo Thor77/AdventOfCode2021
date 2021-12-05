8df33d25cee08c96dba59beffda9e2f9380ba859
Thor77 <thor77@thor77.org>
2021-12-05 11:43:35 +0100

    Modify run.sh to run same script for both parts

--- run.sh
+++ run.sh
@@ -12,12 +12,22 @@ if [ ! -f "$input_path" ]; then echo "$input_path doesn't exist"; exit 1; fi
 
 p1="day${day}p1.awk"
 p2="day${day}p2.awk"
+combined="day${day}.awk"
+
+if [ -f "$combined" ];
+then
+    p1args="-f $combined"
+    p2args="-v part2=1 -f $combined"
+else
+    p1args="-f $p1"
+    p2args="-f $p2"
+fi
 
 echo -n "Part 1: "
-awk -f "$p1" "$input_path"
+awk $p1args "$input_path"
 
-if [ -f "$p2" ];
+if [ -f "$p2" ] || [ -f "$combined" ];
 then
     echo -n "Part 2: "
-    awk -f "$p2" "$input_path"
+    awk $p2args "$input_path"
 fi
